b44a766bf3c9691e19eabaae17840faedbeddfdb
Sunil Karki <karkisunil1200@gmail.com>
2019-11-07 14:08:59 -0600

    Added react-strap

--- src/Components/PicList/PicList.js
+++ src/Components/PicList/PicList.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
-import PiList from './PiList.css';
+import {Container, Row} from 'reactstrap';
+// import PiList from './PiList.css';
 
 import Picture from '../Picture/Picture';
 
@@ -20,14 +21,16 @@ const PicList = () => {
   }, []);
 
   return (
-    <div className='picList'>
-      <Picture
-        picture={pictures.url}
-        explanation={pictures.explanation}
-        title={pictures.title}
-        date={pictures.date}
-      />
-    </div>
+    <Container>
+      <Row>
+        <Picture
+          picture={pictures.url}
+          explanation={pictures.explanation}
+          title={pictures.title}
+          date={pictures.date}
+        />
+      </Row>
+    </Container>
   );
 };
 
--- src/Components/Picture/Picture.js
+++ src/Components/Picture/Picture.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import './PictureStyle.css';
+// import './PictureStyle.css';
 
 const Picture = props => {
   return (
